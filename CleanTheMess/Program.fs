af95e8a3ac1257380448c9c681ad426bae96b903
Ihor Stavytskyi <stavytskiy@dtek.com>
2020-04-11 13:38:35 +0300

    initial commit

---
+++ CleanTheMess/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open System.IO
+open System.Collections.Generic
+
+let getPhrase file countToKeep =
+    let text = File.ReadAllText(file)
+    let countBySymbol = new Dictionary<char, int>()
+    text
+        |> Seq.iter (fun symbol -> 
+            let existingCount = countBySymbol.GetValueOrDefault(symbol, 0)
+            countBySymbol.[symbol] <- existingCount + 1)
+    text
+        |> Seq.filter (fun symbol -> countBySymbol.[symbol] < countToKeep)
+        |> String.Concat
+
+[<EntryPoint>]
+let main argv =
+    getPhrase "mess.txt" 10 |> printfn "%s" 
+    0
